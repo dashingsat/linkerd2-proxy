e9b030a9499cfb0af8b98bc898df9af77a43a6c6
venepall <venepall@arcesium.com>
2022-07-22 13:12:44 +0530

    feat:  rate limit impl

--- linkerd/http-route/src/http/filter/rate_limiter.rs
+++ linkerd/http-route/src/http/filter/rate_limiter.rs
@@ -28,6 +28,19 @@ pub struct Configuration {
     pub duration: Duration
 }
 
+pub fn create_rate_limiter(time_window: Duration, threshold_count: i32, burst_percentage: i32, path: &str)
+{
+    let rate_limiter_key = path.to_string();
+    if RATELIMITER_CACHE.lock().unwrap().get(&rate_limiter_key ).is_none() {
+        let time_window_in_secs = time_window.as_secs();
+        //let max_burst = nonzero!(((burst_percentage as u32)/100)*(*&threshold_count as u32));
+        let quota = Quota::with_period(Duration::from_secs(time_window_in_secs/(threshold_count as u64)))
+            .unwrap();
+        //.allow_burst(max_burst);
+        RATELIMITER_CACHE.lock().unwrap().insert(rate_limiter_key , GovernorRateLimiter::keyed(quota));
+    }
+}
+
 impl<T: Clone> RateLimiter<T> {
     pub fn apply(&self, path: &str) -> Option<T> {
         if !Self::check_for_rate_limiting(path) {
@@ -49,19 +62,6 @@ impl<T: Clone> RateLimiter<T> {
             RATELIMITER_CACHE.lock().unwrap().get("default").unwrap().check_key(&rate_limiter_key) == Ok(())
         } else {
             RATELIMITER_CACHE.lock().unwrap().get(&rate_limiter_key).unwrap().check_key(&rate_limiter_key) == Ok(())
-        }
-    }
-
-    pub fn create_rate_limiter(time_window: Duration, threshold_count: i32, burst_percentage: i32, path: &str)
-    {
-        let rate_limiter_key = path.to_string();
-        if RATELIMITER_CACHE.lock().unwrap().get(&rate_limiter_key ).is_none() {
-            let time_window_in_secs = time_window.as_secs();
-            //let max_burst = nonzero!(((burst_percentage as u32)/100)*(*&threshold_count as u32));
-            let quota = Quota::with_period(Duration::from_secs(time_window_in_secs/(threshold_count as u64)))
-                .unwrap();
-            //.allow_burst(max_burst);
-            RATELIMITER_CACHE.lock().unwrap().insert(rate_limiter_key , GovernorRateLimiter::keyed(quota));
         }
     }
 }
